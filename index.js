0630b6f73068b20d604a4cc57c7e802c5eb8b6fb
GNEHS <jayuiop@gmail.com>
2018-02-07 20:20:41 +0800

    程式碼優化、字串修改

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const fs = require('fs') //檔案系統
-const words2replace = require(__dirname + '/data.js').getTData //翻譯替換檔
+const replaceWords = require(__dirname + '/data.js').getTData //翻譯替換檔
 
+console.log(Date.now())
 console.log('gnehs/TeleReplace')
 console.log('https://github.com/gnehs/TeleReplace')
 
 fs.readdir(__dirname + "/source/", function(err, files) { //讀取資料夾
-    for (var i = 0; i < files.length; i++) {
-
+    if (err) throw err;
+    for (i in files) {
         let fileUrl = __dirname + '/source/' + files[i] //檔案位置
         let fileName = files[i] //檔案名稱
 
         fs.readFile(fileUrl, (err, fileData) => {
             if (err) throw err;
-            for (var i = 0; i < words2replace.length; i++) {
-                fileData = ('' + fileData).replace(new RegExp(words2replace[i][0], "g"), words2replace[i][1]) //取代字詞
-            }
-            saveOutput(fileData, fileName) //存檔
+            saveOutput(replaceWord(fileData), fileName) //存檔
         })
 
     }
 })
 
+function replaceWord(data) {
+    for (i in replaceWords) {
+        data = data.toString().replace(new RegExp(replaceWords[i][0], "g"), replaceWords[i][1]) //取代字詞
+    }
+    return data;
+}
+
 function saveOutput(fileData, fileName) { //存檔
-    fs.writeFile(__dirname + '/output/' + fileName, fileData, (err) => {
-        if (err) throw err;
-        console.log(' - 已轉換 ' + fileName)
-    })
+    fs.writeFile(__dirname + '/output/' + fileName, fileData, (err) => { if (err) throw err })
+    console.log(' - 已轉換 ' + fileName)
 }
